8619f61acbdd6fa9d66488678ecac502a6e6da8d
Tiri <shaymusts@gmail.com>
2021-10-28 15:44:53 +0200

    checking if property exists before rendering it

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -6,7 +6,7 @@ const Post = ({ post }) => {
   const { title, byline, media, published_date } = post
   return (
     <Link to={{ pathname: '/details', state: { post } }} className='rounded-xl w-2/3 shadow-md my-2 px-5 py-2 border cursor-pointer bg-white'>
-      {media[0] && <PostCard title={title} byline={byline} media={media} published_date={published_date}/> }
+      <PostCard title={title} byline={byline} media={media} published_date={published_date}/>
     </Link>
   )
 }
