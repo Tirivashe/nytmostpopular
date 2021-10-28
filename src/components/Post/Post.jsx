138b4bb793bd8d73970b7f1d04a6f033e88deeae
Tiri <shaymusts@gmail.com>
2021-10-28 16:15:03 +0200

    strict check to see if data has media to render

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -4,11 +4,14 @@ import PostCard from '../PostCard/PostCard'
 
 const Post = ({ post }) => {
   const { title, byline, media, published_date } = post
-  return (
+  
+  if(media.length === 1) {return (
     <Link to={{ pathname: '/details', state: { post } }} className='rounded-xl w-2/3 shadow-md my-2 px-5 py-2 border cursor-pointer bg-white'>
       <PostCard title={title} byline={byline} media={media} published_date={published_date}/>
     </Link>
-  )
-}
+    )}else{
+      return null
+    }
+  }
 
 export default Post
